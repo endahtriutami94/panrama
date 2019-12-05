7f5bca897bae611aca433a24c054d7955fbc645e
endahtriutami <endah@tiket.com>
2019-12-05 22:12:40 +0700

    csr

--- csr.php
+++ csr.php
@@ -10,51 +10,85 @@
       practices across all our operations. We have well defined and measurable policies and practices including:
     </div>
 
-    <div class="d-flex flex-column my-4">
-      <div class="d-flex justify-content-center mb-3">
-        <div class="d-flex justify-content-center align-items-center bg-dark-blue p-4 w-25">
-          <img src="assets/images/csr/icon1.png" width="25%" />
+    <div class="d-flex flex-column my-4 align-items-center">
+      <div class="w-75">
+        <div class="d-flex justify-content-center mb-3">
+          <div class="d-flex justify-content-center align-items-center bg-dark-blue p-4">
+            <img src="assets/images/csr/icon1.png" width="50%" />
+          </div>
+          <div class="csr-box p-4">
+            <p class="ml-3">ENERGY MANAGEMENT SYSTEM<p>
+            <ul>
+              <li>Maximum LED lighting</li>
+              <li>Efficient lighting, cooling & ventilation systems</li>
+              <li>Progressive use of energy-saving servo motors in sewing machines</li>
+            </ul>
+          </div>
         </div>
-        <div class="csr-box p-4">
-          <p class="ml-3">ENERGY MANAGEMENT SYSTEM<p>
-          <ul>
-            <li>Maximum LED lighting</li>
-            <li>Efficient lighting, cooling & ventilation systems</li>
-            <li>Progressive use of energy-saving servo motors in sewing machines</li>
-          </ul>
-        </div>
-      </div>
 
-      <div class="d-flex justify-content-center mb-3">
-        <div class="d-flex justify-content-center align-items-center bg-dark-blue p-4 w-25">
-          <img src="assets/images/csr/icon2.png" width="25%" />
+        <div class="d-flex justify-content-center mb-3">
+          <div class="d-flex justify-content-center align-items-center bg-dark-blue p-4">
+            <img src="assets/images/csr/icon2.png" width="50%" />
+          </div>
+          <div class="csr-box p-4">
+            <p class="ml-3">WASTE MANAGEMENT<p>
+            <ul>
+              <li>Segregation of waste</li>
+              <li>Routing paper, plastic & fabric waste to verified recycling facilities</li>
+              <li>Chemical waste – separate containment area</li>
+            </ul>
+          </div>
         </div>
-        <div class="csr-box p-4">
-          <p class="ml-3">WASTE MANAGEMENT<p>
-          <ul>
-            <li>Segregation of waste</li>
-            <li>Routing paper, plastic & fabric waste to verified recycling facilities</li>
-            <li>Chemical waste – separate containment area</li>
-          </ul>
-        </div>
-      </div>
-      
-      <div class="d-flex justify-content-center mb-3">
-        <div class="d-flex justify-content-center align-items-center bg-dark-blue p-4 w-25">
-          <img src="assets/images/csr/icon3.png" width="25%" />
-        </div>
-        <div class="csr-box p-4">
-          <p class="ml-3">GOING GREEN<p>
-          <ul>
-            <li>“Save our Trees” drive, going paperles, printing only when necessary</li>
-            <li>Recycle & reuse paper</li>
-            <li>Encouraging use of re-usable containers, phasing out single-use plastic</li>
-          </ul>
+        
+        <div class="d-flex justify-content-center mb-3">
+          <div class="d-flex justify-content-center align-items-center bg-dark-blue p-4">
+            <img src="assets/images/csr/icon3.png" width="50%" />
+          </div>
+          <div class="csr-box p-4">
+            <p class="ml-3">GOING GREEN<p>
+            <ul>
+              <li>“Save our Trees” drive, going paperles, printing only when necessary</li>
+              <li>Recycle & reuse paper</li>
+              <li>Encouraging use of re-usable containers, phasing out single-use plastic</li>
+            </ul>
+          </div>
         </div>
       </div>
 
     </div>
 
+    <div class="text-center">
+      <h1 class="text-title">Community</h1>
+      <p class="mb-4">Giving back to our employees and the community</p>
+
+      <div class="d-flex">
+        <div class="d-flex flex-column mr-4">
+          <img src="assets/images/csr/comunity1.png" width="100%" />
+          <img src="assets/images/csr/comunity2.png" width="100%" />
+        </div>
+
+        <div class="d-flex flex-column">
+          <img src="assets/images/csr/comunity3.png" width="100%" />
+          <img src="assets/images/csr/comunity4.png" width="100%" />
+        </div>
+
+      </div>
+
+      <div class="border my-3"></div>
+
+      <div class="d-flex">
+        <div class="d-flex flex-column mr-4">
+          <img src="assets/images/csr/comunity5.png" width="100%" />
+          <img src="assets/images/csr/comunity6.png" width="100%" />
+        </div>
+
+        <div class="d-flex flex-column">
+          <img src="assets/images/csr/comunity7.png" width="100%" />
+          <img src="assets/images/csr/comunity8.png" width="100%" />
+        </div>
+      </div>
+
+    </div>
   </div>
 
 </div>
